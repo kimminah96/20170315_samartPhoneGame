b9377ab0af1057f801df87aec4b3fa5de26dec1e
KPUGame <kpugame@KPUGameui-MacBook-Pro-2.local>
2017-03-15 16:42:11 +0900

    Initial Commit

---
+++ 20170315_samartPhoneGame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  20170315_samartPhoneGame
+//
+//  Created by KPUGame on 2017. 3. 15..
+//  Copyright © 2017년 KPUGame. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
